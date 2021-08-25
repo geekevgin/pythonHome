5fa5957fbaf0caaa1808246afa24505a2bcf55ea
geekevgin <evgenia.belousova@gmail.com>
2021-08-25 16:03:57 +0300

    lesson3 task5

--- lesson3/task5.py
+++ lesson3/task5.py
@@ -4,10 +4,14 @@ def my_list_of_numbers():
         str_of_numbers = input('Enter the list of numbers:')
         list_of_numbers = str_of_numbers.split()
         for i in list_of_numbers:
-            if i.isdigit():
-                sum = sum + int(i)
-                return sum
-        else:
-            print('You have entered the symbol')
+            try:
+                if i == ' ':
+                    print(f'sum equals {sum}.')
+                    return
+                else:
+                    sum = sum + int(i)
+            except ValueError:
+                return ValueError
+                print('Enter any number')
+        print(f'sum equals {sum}')
 
-print(my_list_of_numbers())
