af104076a26622b1885098969f3139d6bf8e74d6
geekevgin <evgenia.belousova@gmail.com>
2021-08-25 10:58:19 +0300

    lesson3 task5

--- lesson3/task5.py
+++ lesson3/task5.py
@@ -1,14 +1,13 @@
-def my_sum_list():
+def my_list_of_numbers():
     sum = 0
-    while sum != 0:
+    while sum != '':
         str_of_numbers = input('Enter any list of numbers:')
         list_of_numbers = str_of_numbers.split()
-    for i in list_of_numbers:
-        if i.isdigit():
-            sum += int(i)
-            return sum
-            print(sum)
-    else:
-        print('You have entered the symbol')
-        sum = ''
-print(sum)
+        for i in list_of_numbers:
+            if i.isdigit():
+                sum = sum + int(i)
+                return sum
+        else:
+            print('You have entered the symbol')
+
+print(my_list_of_numbers())
