42e02687c587ab98e9a691834cc3760e4b3787e4
geekevgin <evgenia.belousova@gmail.com>
2021-08-25 11:01:31 +0300

    lesson3 task4

--- lesson3/task3.py
+++ lesson3/task3.py
@@ -1,5 +1,5 @@
-def my_func_sum(num_1, num_2, num_3):
-    res_my_func_sum = [num_1, num_2, num_3]
-    res_my_func_sum.remove(min(num_1, num_2, num_3))
-    return sum(res_my_func_sum)
-print(my_func_sum(5, 4, 6))
+def my_func_sum_1(num_1, num_2, num_3):
+    res_my_func_sum_1 = [num_1, num_2, num_3]
+    res_my_func_sum_1.remove(min(num_1, num_2, num_3))
+    return sum(res_my_func_sum_1)
+print(my_func_sum_1(5, 4, 6))
