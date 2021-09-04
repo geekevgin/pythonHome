742433c8d117f58ca4b76e1c048cd5a7cb265573
geekevgin <evgenia.belousova@gmail.com>
2021-09-04 20:14:10 +0300

    lesson5 task1

--- lesson5/task3.py
+++ lesson5/task3.py
@@ -1,12 +1,12 @@
 with open('text_task3.txt') as file:
     file_lines = file.readlines()
     workers = {}
-    total_salary = 0
+    total_sal = 0
 for line in file_lines:
     database_of_workers = line.split()
     workers.update({database_of_workers[0]: float(database_of_workers[1])})
-    total_salary = total_salary + float(database_of_workers[1])
-average_salary = total_salary / len(workers)
+    total_sal = total_sal + float(database_of_workers[1])
+average_salary = total_sal / len(workers)
 print(average_salary)
 for w, s in workers.items():
     if s < 20000:
